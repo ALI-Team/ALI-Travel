c2a5cc1aa54ebfc7137b49ee048f2a71c11d3d0a
Luka Jankovic <lukjan1999@gmail.com>
2017-03-04 23:10:30 +0100

    better dialog

--- app/src/main/java/alitea/am/ali_travel/SearchActivity.java
+++ app/src/main/java/alitea/am/ali_travel/SearchActivity.java
@@ -1,9 +1,16 @@
 package alitea.am.ali_travel;
 
+import android.content.res.ColorStateList;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -17,9 +24,45 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
 
-        //Initialize spinner
-        MultiSelectionSpinner trafikslag = (MultiSelectionSpinner) findViewById(R.id.spinner);
-        trafikslag.setItems(Arrays.asList(getResources().getStringArray(R.array.transport_types)));
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.modes_of_transport) {
+            new MaterialDialog.Builder(this)
+                    .title(R.string.modes_of_transport)
+                    .items(R.array.transport_types)
+                    .itemsCallbackMultiChoice(null, new MaterialDialog.ListCallbackMultiChoice() {
+                        @Override
+                        public boolean onSelection(MaterialDialog dialog, Integer[] which, CharSequence[] text) {
+                            /**
+                             * If you use alwaysCallMultiChoiceCallback(), which is discussed below,
+                             * returning false here won't allow the newly selected check box to actually be selected
+                             * (or the newly unselected check box to be unchecked).
+                             * See the limited multi choice dialog example in the sample project for details.
+                             **/
+                            return true;
+                        }
+                    })
+                    .positiveText(R.string.choose)
+                    .positiveColor(getResources().getColor(R.color.colorPrimary))
+                    .choiceWidgetColor(ColorStateList.valueOf(getResources().getColor(R.color.colorPrimary)))
+                    .show();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
