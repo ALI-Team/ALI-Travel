becba58e17c07898ed7346b221abfbbbcad1573c
Luka Jankovic <lukjan1999@gmail.com>
2017-03-04 23:51:47 +0100

    removed unnecesary files

--- app/src/main/java/alitea/am/ali_travel/SearchActivity.java
+++ app/src/main/java/alitea/am/ali_travel/SearchActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
@@ -15,8 +16,6 @@ import com.afollestad.materialdialogs.MaterialDialog;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
-import alitea.am.ali_travel.ui_elements.MultiSelectionSpinner;
 
 public class SearchActivity extends AppCompatActivity {
 
