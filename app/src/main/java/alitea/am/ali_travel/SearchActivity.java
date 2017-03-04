aa0695dbf9da79a460543e6fccf98b2af4a232c5
Luka Jankovic <lukjan1999@gmail.com>
2017-03-04 22:14:49 +0100

    added menu

--- app/src/main/java/alitea/am/ali_travel/SearchActivity.java
+++ app/src/main/java/alitea/am/ali_travel/SearchActivity.java
@@ -2,6 +2,14 @@ package alitea.am.ali_travel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import alitea.am.ali_travel.ui_elements.MultiSelectionSpinner;
 
 public class SearchActivity extends AppCompatActivity {
 
@@ -9,5 +17,9 @@ public class SearchActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        //Initialize spinner
+        MultiSelectionSpinner trafikslag = (MultiSelectionSpinner) findViewById(R.id.spinner);
+        trafikslag.setItems(Arrays.asList(getResources().getStringArray(R.array.transport_types)));
     }
 }
