cf201fee364ba53c61f858893db1240138ba6361
Luka Jankovic <lukjan1999@gmail.com>
2017-03-09 19:13:56 +0100

    added departure / arrival radio buttons

--- app/src/main/java/alitea/am/ali_travel/SearchActivity.java
+++ app/src/main/java/alitea/am/ali_travel/SearchActivity.java
@@ -1,19 +1,30 @@
 package alitea.am.ali_travel;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.res.ColorStateList;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.TimePicker;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.List;
 
 import alitea.am.ali_travel.ui_elements.MultiSelectionSpinner;
@@ -65,5 +76,117 @@ public class SearchActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        final RadioButton rbutton = (RadioButton)view;
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.departure_date_radio:
+                if (checked) {
+
+                    RadioButton arrival = (RadioButton)findViewById(R.id.arrival_date_radio);
+                    arrival.setChecked(false);
+                    arrival.setText(getString(R.string.arrival_date));
+
+                    final int mYear, mMonth, mDay, mHour, mMinute;
+                    final Calendar c = Calendar.getInstance();
+
+                    mYear = c.get(Calendar.YEAR);
+                    mMonth = c.get(Calendar.MONTH);
+                    mDay = c.get(Calendar.DAY_OF_MONTH);
+                    mHour = c.get(Calendar.HOUR_OF_DAY);
+                    mMinute = c.get(Calendar.MINUTE);
+
+                    final Context that = (Context)this;
+
+                    DatePickerDialog date = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, final int month, final int dayOfMonth) {
+                            TimePickerDialog time = new TimePickerDialog(that, new TimePickerDialog.OnTimeSetListener() {
+                                @Override
+                                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                    rbutton.setText(getString(R.string.departure_date)+" - "+month+"/"+dayOfMonth+" "+hourOfDay+":"+minute);
+                                }
+                            }, mHour, mMinute, false);
+
+                            time.setButton(DialogInterface.BUTTON_NEGATIVE, getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if (which == DialogInterface.BUTTON_NEGATIVE) {
+                                        rbutton.setChecked(false);
+                                    }
+                                }
+                            });
+
+                            time.show();
+                        }
+                    }, mYear, mMonth, mDay);
+
+                    date.setButton(DialogInterface.BUTTON_NEGATIVE, getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (which == DialogInterface.BUTTON_NEGATIVE) {
+                                rbutton.setChecked(false);
+                            }
+                        }
+                    });
+
+                    date.show();
+                }
+                break;
+            case R.id.arrival_date_radio:
+                if (checked) {
+                    RadioButton departure = (RadioButton)findViewById(R.id.departure_date_radio);
+                    departure.setChecked(false);
+                    departure.setText(getString(R.string.departure_date));
+
+                    final int mYear, mMonth, mDay, mHour, mMinute;
+                    final Calendar c = Calendar.getInstance();
+
+                    mYear = c.get(Calendar.YEAR);
+                    mMonth = c.get(Calendar.MONTH);
+                    mDay = c.get(Calendar.DAY_OF_MONTH);
+                    mHour = c.get(Calendar.HOUR_OF_DAY);
+                    mMinute = c.get(Calendar.MINUTE);
+
+                    final Context that = (Context)this;
+
+                    DatePickerDialog date = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, final int month, final int dayOfMonth) {
+                            TimePickerDialog time = new TimePickerDialog(that, new TimePickerDialog.OnTimeSetListener() {
+                                @Override
+                                public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                    rbutton.setText(getString(R.string.arrival_date)+" - "+month+"/"+dayOfMonth+" "+hourOfDay+":"+minute);
+                                }
+                            }, mHour, mMinute, false);
+
+                            time.setButton(DialogInterface.BUTTON_NEGATIVE, getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if (which == DialogInterface.BUTTON_NEGATIVE) {
+                                        rbutton.setChecked(false);
+                                    }
+                                }
+                            });
+
+                            time.show();
+                        }
+                    }, mYear, mMonth, mDay);
+
+                    date.setButton(DialogInterface.BUTTON_NEGATIVE, getString(android.R.string.cancel), new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            if (which == DialogInterface.BUTTON_NEGATIVE) {
+                                rbutton.setChecked(false);
+                            }
+                        }
+                    });
+
+                    date.show();
+                }
+                    break;
+        }
     }
 }
